841c68d85b18460884f58efa193d9f0ddd0619ba
zhazhachua <49892786+zhazhachua@users.noreply.github.com>
2022-09-19 21:06:58 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/download.jpg') {
+        myImage.setAttribute('src', 'images/tmp-use.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/download.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('请输入你的名字');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Moziilla酷毙了，' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+ }
